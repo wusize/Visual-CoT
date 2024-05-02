b7e1d177155348073e7745eb4769cc7adcfed9d3
wusize <wusize99@gmail.com>
2024-05-02 16:49:58 +0800

    collect_test_images

---
+++ collect_images.py
@@ -0,0 +1,31 @@
+import json
+import argparse
+import os
+from tqdm import tqdm
+import shutil
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--json_path', default='viscot_benchmark/benchmark/sroie.json', type=str)
+    parser.add_argument('--src_path', default='playground/data', type=str)
+    parser.add_argument('--dst_path', default='data', type=str)
+    args = parser.parse_args()
+
+    with open(args.json_path, 'r') as f:
+        data = json.load(f)
+
+    for data_sample in tqdm(data):
+        src_file = os.path.join(args.src_path, data_sample['image'][0])
+        assert os.path.exists(src_file)
+        dst_file = os.path.join(args.dst_path, data_sample['image'][0])
+        shutil.copyfile(src_file, dst_file)
+
+
+
+
+
+
+
+
+
